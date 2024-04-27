f1b371e8e736ea72b1c0327280b84a728c5ab91e
Renato Ganske Junior <renatoganske.jr@gmail.com>
2024-04-26 23:46:07 -0300

    Refatorando o código para salvar as contas no banco de dados.

--- byte-bank-alura-curso_jdbc_inicio/src/main/java/br/com/alura/bytebank/domain/conta/ContaService.java
+++ byte-bank-alura-curso_jdbc_inicio/src/main/java/br/com/alura/bytebank/domain/conta/ContaService.java
@@ -1,13 +1,23 @@
 package br.com.alura.bytebank.domain.conta;
 
+import br.com.alura.bytebank.ConnectionFactory;
 import br.com.alura.bytebank.domain.RegraDeNegocioException;
 import br.com.alura.bytebank.domain.cliente.Cliente;
 
 import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.Set;
 
 public class ContaService {
+
+    private ConnectionFactory connection;
+
+    public ContaService() {
+        this.connection = new ConnectionFactory();
+    }
 
     private Set<Conta> contas = new HashSet<>();
 
@@ -27,7 +37,25 @@ public class ContaService {
             throw new RegraDeNegocioException("Já existe outra conta aberta com o mesmo número!");
         }
 
-        contas.add(conta);
+        String sql = "INSERT INTO conta (numero, saldo, cliente_nome, cliente_cpf, cliente_email) " +
+                "VALUES (?, ?, ?, ?, ?)";
+
+        Connection conn = connection.recuperarConexao();
+
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = conn.prepareStatement(sql);
+
+            preparedStatement.setInt(1,conta.getNumero());
+            preparedStatement.setBigDecimal(2,BigDecimal.ZERO);
+            preparedStatement.setString(3, cliente.getNome());
+            preparedStatement.setString(4, cliente.getCpf());
+            preparedStatement.setString(5, cliente.getEmail());
+
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void realizarSaque(Integer numeroDaConta, BigDecimal valor) {
